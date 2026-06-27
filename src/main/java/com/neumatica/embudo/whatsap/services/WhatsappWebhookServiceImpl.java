40df3dd742c6fe32e18526ada41afbce7032b215
Andres30211 <felifit27@gmail.com>
2026-06-27 14:54:13 -0500

    Probar un webhook para validar meta y guardar en la base de datos de render

--- src/main/java/com/neumatica/embudo/whatsap/services/WhatsappWebhookServiceImpl.java
+++ src/main/java/com/neumatica/embudo/whatsap/services/WhatsappWebhookServiceImpl.java
@@ -41,7 +41,7 @@ public class WhatsappWebhookServiceImpl implements  WhatsappWebhookService{
 	
 	@Override
 	public List<Contact> contacts() {
-		return this.contactRepository.findAll();
+		return this.contactRepository.findAllWithConversations();
 	}
 
 	@Override
