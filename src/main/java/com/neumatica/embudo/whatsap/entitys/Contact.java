5751d50722ab59113420b465f6ed7c06d3fbe726
Andres30211 <felifit27@gmail.com>
2026-06-27 11:00:28 -0500

    Probar un webhook para validar meta y guardar en la base de datos de render

--- src/main/java/com/neumatica/embudo/whatsap/entitys/Contact.java
+++ src/main/java/com/neumatica/embudo/whatsap/entitys/Contact.java
@@ -1,13 +1,20 @@
 package com.neumatica.embudo.whatsap.entitys;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -58,6 +65,10 @@ public class Contact {
      * País
      */
     private String country;
+    
+    @JsonBackReference
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Conversation> conversations = new ArrayList<>();
 
     /**
      * Primer contacto
