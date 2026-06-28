26daff7af4a4a7f7b94d98a4ae463d85fc8158f0
Andres30211 <felifit27@gmail.com>
2026-06-28 11:12:52 -0500

    Probar un webhook para validar meta y guardar en la base de datos de render

--- src/main/java/com/neumatica/embudo/whatsap/controller/WebhookTwilioController.java
+++ src/main/java/com/neumatica/embudo/whatsap/controller/WebhookTwilioController.java
@@ -30,6 +30,7 @@ public class WebhookTwilioController {
     public ResponseEntity<String> webhook(
             @RequestBody WhatsappWebHookDto webhook) {
 		
+		System.out.println(webhook);
 		this.whatsappWebhookService.processWebhook(webhook);
 
         return ResponseEntity.ok().build();
